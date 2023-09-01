ceff7d505f49a7fd5f68b8710199f7044b9bd902
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-09-01 13:56:09 -0500

    API gateway added
    API gateway added

---
+++ inventory-service/src/test/java/com/shamstabrez16/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shamstabrez16.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
